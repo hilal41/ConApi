66e870b87aaa9b96f320e2ebf5154c8d1768d3f2
Hilal PC <Hilal PC@DESKTOP-LPSLHNS>
2025-03-02 13:25:01 +0500

    Add project files.

---
+++ Model/Domain/Contact.cs
@@ -0,0 +1,16 @@
+﻿namespace ConApi.Model.Domain
+{
+    public class Contact
+    {
+        public Guid Id { get; set; }
+
+        public required string  Name { get; set; }
+
+        public string? Email { get; set; }
+
+        public required string PhoneNumber  { get; set; }
+
+        public bool Favorite { get; set; }
+         
+    }
+}
